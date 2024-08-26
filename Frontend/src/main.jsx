8a267ae4b1bcafbab6429ea12117a449e10632f9
Riya <120216825+RiyaDv@users.noreply.github.com>
2024-08-26 00:16:54 -0400

    Add files via upload

---
+++ Frontend/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import { Provider } from "react-redux";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import store from "./redux/Store/store.js";
+
+const qureyClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={qureyClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
+  </StrictMode>
+);
